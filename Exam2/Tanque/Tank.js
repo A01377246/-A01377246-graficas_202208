a109ec44d1be4f9616804b8b80f4c540932fd422
Humberto Carrillo <A01377246@tec.mx>
2022-10-23 18:31:48 -0500

    tank without light

--- Exam2/Tanque/Tank.js
+++ Exam2/Tanque/Tank.js
@@ -2,7 +2,7 @@
 
 import * as THREE from "./libs/three.module.js";
 import { OrbitControls } from "./libs/controls/OrbitControls.js";
-import { OBJLoader } from "./libs/loaders/OBJLoader";
+import { OBJLoader } from "./libs/loaders/OBJLoader.js";
 import { GUI } from "./libs/dat.gui.module.js";
 
 let renderer = null,
@@ -12,10 +12,22 @@ let renderer = null,
 
 let ambientLight = null;
 
+let tankObjectList = [];
+
+let turretObjectList = [];
+
 let settings = null;
 
 let mapUrl = "./checker_large.gif";
 
+let tankObjModelUrl = {
+  obj: "./Tank/Tank.obj",
+  textureMap: "./Tank/Tank_texture.jpg",
+};
+let turretObjModelUrl = {
+  obj: "./Tank/Turret.obj",
+  textureMap: "./Tank/Tank_texture.jpg",
+};
 const createTankGui = () => {
   const tankGui = new GUI({ width: 200 });
 
@@ -43,7 +55,48 @@ async function loadObj(objModelUrl, objectList) {
       ? new THREE.TextureLoader().load(objModelUrl.specularMap)
       : null;
 
-    console.log(object);
+    // object.traverse(function (child)
+    // {
+    for (const child of object.children) {
+      //     if (child.isMesh)
+      child.castShadow = true;
+      child.receiveShadow = true;
+      child.material.map = texture;
+      child.material.normalMap = normalMap;
+      child.material.specularMap = specularMap;
+    }
+    // });
+
+    object.scale.set(3, 3, 3);
+    object.position.z = -3;
+    object.position.x = -1.5;
+    object.rotation.y = -6;
+    object.name = "objObject";
+
+    objectList.push(object);
+    scene.add(object);
+  } catch (err) {
+    onError(err);
+  }
+}
+
+async function loadTurretObj(objModelUrl, objectList) {
+  try {
+    const object = await new OBJLoader().loadAsync(
+      objModelUrl.obj,
+      onProgress,
+      onError
+    );
+
+    let texture = objModelUrl.hasOwnProperty("normalMap")
+      ? new THREE.TextureLoader().load(objModelUrl.map)
+      : null;
+    let normalMap = objModelUrl.hasOwnProperty("normalMap")
+      ? new THREE.TextureLoader().load(objModelUrl.normalMap)
+      : null;
+    let specularMap = objModelUrl.hasOwnProperty("specularMap")
+      ? new THREE.TextureLoader().load(objModelUrl.specularMap)
+      : null;
 
     // object.traverse(function (child)
     // {
@@ -60,7 +113,8 @@ async function loadObj(objModelUrl, objectList) {
     object.scale.set(3, 3, 3);
     object.position.z = -3;
     object.position.x = -1.5;
-    object.rotation.y = -3;
+    object.position.y = 1.1;
+    object.rotation.y = -6;
     object.name = "objObject";
 
     objectList.push(object);
@@ -78,6 +132,20 @@ function main() {
   createTankGui();
 
   update();
+}
+
+function onError(err) {
+  console.error(err);
+}
+
+function onProgress(xhr) {
+  if (xhr.lengthComputable) {
+    const percentComplete = (xhr.loaded / xhr.total) * 100;
+    console.log(
+      xhr.target.responseURL,
+      Math.round(percentComplete, 2) + "% downloaded"
+    );
+  }
 }
 
 function update() {
@@ -125,6 +193,10 @@ async function createScene(canvas) {
   mesh.castShadow = false;
   mesh.receiveShadow = true;
   scene.add(mesh);
+
+  // Creting the objects
+  loadObj(tankObjModelUrl, tankObjectList);
+  loadTurretObj(turretObjModelUrl, turretObjectList);
 }
 
 main();
